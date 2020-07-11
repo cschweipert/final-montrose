9e549b8f93971cdf5e17c52ed641dd80223ca86a
Christina Schweipert <c.schweipert@gmail.com>
2020-07-10 20:55:09 -0600

    Create pages and components

---
+++ frontend/components/Header.js
@@ -0,0 +1,12 @@
+import Nav from './Nav';
+
+const Header = () => (
+    <div>
+        <div className="">
+            <h1>Montrose Community Board</h1>
+            <Nav />
+        </div>
+    </div>
+);
+
+export default Header;
