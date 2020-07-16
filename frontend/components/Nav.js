ae1b81b5eb64e702df1faaa7e1546319cfcaaa0e
Christina Schweipert <c.schweipert@gmail.com>
2020-07-16 12:52:54 -0600

    Render items

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -5,10 +5,21 @@ const Nav = () => (
         <Link href="/">
             <a>Home </a>
         </Link>
-        <Link href="/categories">
+        <Link href="/items">
             <a>Categories</a>
+        </Link>
+        <Link href="/post">
+            <a>Post</a>
+        </Link>
+        <Link href="/signup">
+            <a>Signup</a>
+        </Link>
+        <Link href="/me">
+            <a>Account</a>
         </Link>
     </div>
 );
 
 export default Nav;
+
+
