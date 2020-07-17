311fdb2e8c981e83711324ea608515fae88927fa
Christina Schweipert <c.schweipert@gmail.com>
2020-07-17 13:40:20 -0600

    update item

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,6 +1,5 @@
 const Mutations = {
     async createItem(parent, args, ctx, info) {
-
         const item = await ctx.db.mutation.createItem(
             {
                 data: {
@@ -14,7 +13,22 @@ const Mutations = {
 
         return item;
     },
-
+    updateItem(parent, args, ctx, info) {
+        // first take a copy of the updates
+        const updates = { ...args };
+        // remove the ID from the updates
+        delete updates.id;
+        // run the update method
+        return ctx.db.mutation.updateItem(
+            {
+                data: updates,
+                where: {
+                    id: args.id,
+                },
+            },
+            info
+        );
+    },
 };
 
 module.exports = Mutations;
